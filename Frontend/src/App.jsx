83e1ad6617f1b27d31188b7f0766bce3c0b16e9e
adityajha <aj12102003@gmail.com>
2025-08-17 11:47:02 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-center text-5xl text-red-600'>Start your Work</h1>
+    </div>
+  )
+}
+
+export default App
